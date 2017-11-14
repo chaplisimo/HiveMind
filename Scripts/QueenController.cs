182f8de2a0e622faaa34c33c6dda42088937b3ee
Hugo Puyol <hugo_asterix@hotmail.com>
2017-11-13 22:37:51 -0300

    Update QueenController.cs

--- Scripts/QueenController.cs
+++ Scripts/QueenController.cs
@@ -3,14 +3,35 @@ using UnityEngine;
 public class QueenController : MonoBehaviour {
   
   public GameObject antPrefab;
+  public int antResourceCost = 2;
+  
   public float breedTime = 2;
+  
+  public int resourcesLeft = 5;
+  
+  public bool isBreeding = true;
   
   void Start(){
     StartCoroutine(BreedAnts());
   }
   
   IEnumerator BreedAnts(){
-    GameObject newAnt = GameObject.Instantiate(antPrefab,transform.postion, transform.rotation);
-    yield return WaitForSeconds(breedTime);
+    if(resourcesLeft >= antResourcesCost){
+      resources -= antResourcesCost;
+      GameObject newAnt = GameObject.Instantiate(antPrefab,transform.postion, transform.rotation);
+      yield return WaitForSeconds(breedTime);
+    } else {
+      StopCoroutine(BreedAnts);
+      isBreeding = false;
+      return null;
+    }
+  }
+  
+  void FeedResources(int resources){
+    resourcesLeft += resources;
+    if(resourcesLeft >= antResourcesCost && !isBreeding){
+      isBreeding = true;
+      StartCoroutine(BreedAnts());
+    }
   }
 }
