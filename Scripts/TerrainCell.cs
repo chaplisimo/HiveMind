d027e120d767c32a7d7801e0f027f392948b77af
Hugo Puyol <hugo_asterix@hotmail.com>
2017-11-17 20:00:07 +0000

    Update TerrainCell.cs

--- Scripts/TerrainCell.cs
+++ Scripts/TerrainCell.cs
@@ -1,12 +1,5 @@
 using UnityEngine;
 
 public class TerrainCell {
-	
-	public Vector3[] vertices;
-	public int[] triangles;
-	
-	public TerrainCell(int i, int j, float cellRadius){
-		//Make the cell
-		//multiply by i & j, and scale upto cellradius
-	}
+
 }
