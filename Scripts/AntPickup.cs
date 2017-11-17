bf76b42d8709b34ed24610fd855f621e11cddfe3
Hugo Puyol <hugo_asterix@hotmail.com>
2017-11-17 18:25:54 +0000

    Update AntPickup.cs

--- Scripts/AntPickup.cs
+++ Scripts/AntPickup.cs
@@ -3,24 +3,19 @@ using System.Collections.Generic;
 
 public class AntPickup : MonoBehaviour {
   
-  public List<GameObject> inventory;
+	public int resources = 0;
   
-  void OnTriggerEnter(Collider other){
-    if(other.CompareTag("Pickable")){
-      //Direct Pickup
-      inventory.Add(other.gameObject);
-      other.transform.parent = gameObject.transform;
-      other.gameObject.SetActive(false);
-    }else 
-      if(other.CompareTag("Queen")){
-		  if(inventory.Count > 0){
-				other.gameObject.GetComponent<QueenController>().FeedResources(inventory.Count);
-        
-				foreach(GameObject gO in inventory){
-				  inventory.Remove(gO);
-				  Destroy(gO); 
-				}
+	void OnTriggerEnter(Collider other){
+		if(other.CompareTag("Resource")){
+			//Direct Pickup
+			ResourceScript rs = other.GetComponent<ResourceScript>();
+			resources += rs.qty;
+			rs.Die();
+		}else if(other.CompareTag("Queen")){
+			if(resources > 0){
+				other.gameObject.GetComponent<QueenController>().FeedResources(resources);
+				resources = 0;
 			}
-      }
+		}
   }
 }
